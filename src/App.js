7891883f5064fbc4662513c34669e29f35cf9107
Okafor Jeffrey <oka4jeffrey1234@gmail.com>
2022-08-04 06:53:06 +0100

    update done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from 'react'
     <form>
       <div>
         <div>
-          <label>Town</label>
+          <label>Food</label>
           <input type='text' />
         </div>
       </div>
